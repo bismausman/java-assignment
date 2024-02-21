beb1e1295ac8e2e6132d5315c9eec7207ccc93b1
Bisma usman <160749493+bismausman@users.noreply.github.com>
2024-02-21 09:26:59 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,251 @@
+// CHAPTER 1 (ALERT)
+
+
+// alert("BISMA USMAN.");
+
+// alert("SHEIKH USMAN.")
+
+
+// alert("bismausman2004@gmail.com.")
+
+// alert("924-25678963.")
+
+// alert("**********")
+
+// QUESTRION NO 2
+// alert("You are learning JavaScript!");
+
+// QUESTION NO 3
+
+// alert("Hello! Welcome to JAVA WORLD. Have a great day!");
+
+
+
+// CHAPTER 2 (VARIABLES FOR STRINGS).......
+
+
+
+
+// (QUESTION NO 1)
+// var a =("charlie var");
+// console.log(a)
+
+
+
+// QUESTION NO 3
+
+// Declare the variable teamName
+// var  teamName;
+
+// Assign a string value to the variable
+// teamName = "CodeCrafters";
+
+// // Display an alert with the team name
+// console.log("Our team name is: " + teamName);
+// alert("Our team name is: " + teamName);
+
+// QUESTION NO 4
+
+// assign a string to the variable bestMan
+// var bestMan = "Charlie";
+
+// alert with the initial value
+// alert("The best man initially is: " + bestMan);
+
+// Reassign the variable with a new string value
+// bestMan = "John";
+
+// Display an alert with the updated value
+// alert("The new best man is: " + bestMan);
+// console.log("The new best man is: " + bestMan);
+
+
+
+
+// Chapter 3 (Variables for numbers)
+
+// QUESTION NO 1
+ // Declare the variable caseQty
+//  var  caseQty;
+
+ // You can assign a value later if needed
+//  caseQty = 10;
+//  alert("The quantity of cases is: " + caseQty);
+//  console.log("The quantity of cases is: " + caseQty);
+
+// QUESTION NO 2
+
+//   variable caseQty
+//  var CaseQty = 144;
+//  console.log(CaseQty);
+
+// 
+//  Display an alert with the variable's value
+//  alert("The quantity of cases is: " + caseQty);
+//  console.log("The quantity of cases is: " + caseQty);
+
+// QUESTION NO 3
+
+  // Original statement
+//   var numString = "9";
+
+  // Convert the string to an 
+//   var num = parseInt(numString);
+
+  //  math operation
+//   var result = num + 5;
+
+  //  result
+//   console.log("Result: " + result);
+
+
+// QUESTION NO 4
+
+// # Declare two numbers
+// number1 = 3
+// number2 = 5
+
+// # Declare a variable
+// result = 0
+
+//  sum of 2 numbers
+// result = number1 + number2
+
+//  numbers and the result
+// console.log("The sum of", number1, "and", number2, "is:", result)
+
+
+// QUESTION NO 5
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+
+// console.log("The order total is:", orderTotal);
+
+// question no 6
+
+
+// number =  + 6
+
+
+// number = number + 8
+
+
+// console.log("The updated value is:", number)
+                             
+
+                                                        // (// Assinment done).......
+
+
+
+
+
+
+//  Task Chapter no 1 (Alert) 
+// QUESTION
+
+// alert(" JavaScript Alert !");
+
+
+// QUESTION
+//  
+//  var message = "JavaScript Alert.\n\n";
+//  message += "Line 1: Welcome to Js Land!\n";
+//  message += "Line 2: Happy Coading.\n";
+//
+//  document.write(message.replace(/\n/g, "<br>"));
+
+// // QUESTION 
+// console.log("i can run js through my web browser's console");
+
+
+// QUESTION 
+// Declare a variable called username
+// var username;
+
+// Assign a value to the username variable
+// username = "Alishba ra";
+
+// Display the username
+// alert("Alishba ra", username);
+
+// QUESTION
+
+    // var myName = "Bisma sheikh ";
+
+    // put the value of myName
+    // alert("Bisma sheikh:", myName);
+
+    // QUESTION
+
+ // a) Declare a JS variable titled message
+//  var message;
+
+ // b) Assign “Hello World” to variable message
+//  message = "Hello World";
+
+ // c) Display the message in an alert box
+//  alert(message);
+
+
+// QUESTION
+
+// Save student's bio data in JS variables
+// var studentName = "ABDUL RAFEY";
+// var studentAge = 87;
+// var studentGrade = "A+";
+
+
+// alert("Student Name: " + studentName);
+// alert("Student Age: " + studentAge);
+// alert("Student Grade: " + studentGrade);
+
+
+// QUESTION
+ // Define a JavaScript variable with a multi-line message
+//  var alertMessage = "pizza!\n";
+//  alertMessage += "Pizzz,\n";
+//  alertMessage += "pizz.\n";
+//  alertMessage += "piz!";
+
+//  // Display the alert with the multi-line message
+//  alert(alertMessage);
+
+
+
+// QUESTION
+//  email address
+//  var email = "example@example.com";
+
+//  //  alert box using string concatenation
+//  alert("Your email address is: " + email);
+
+// QUESTION
+ // a book title
+//  var book = "A smarter\n way to learn JavaScript";
+
+//  // a message in an alert box
+//  alert("I am currently reading the book:" + book);
+
+// QUESTION
+
+            //   CHAPTER NO 3 
+
+            // QUESTION
+
+  //  it your age
+//   var age = 25; // Replace with your actual age
+
+//   //  age in an alert box
+//   alert("MY age is: " + age);
+
+
+// QUESTION
+
+ 
+//  var visitorName = "John Doe";
+//  var productTitle = "T-shirt";
+//  var quantity = 5;
+
+//  document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.");
